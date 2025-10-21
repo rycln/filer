63f371dd7d4ba009cc616bd3091ba04b3dcb6dca
Khalikov Ruslan <rycln1@gmail.com>
2025-10-21 11:03:18 +0300

    feat: added tui view

---
+++ internal/tui/view.go
@@ -0,0 +1,55 @@
+package tui
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (m Model) View() string {
+	switch m.state {
+	case FileManageState:
+		return m.fileManageView()
+	case ProcessingState:
+		return m.processingView()
+	case EndState:
+		return m.endView()
+	case ErrorState:
+		return m.errorView()
+	}
+
+	return ""
+}
+
+func (m Model) fileManageView() string {
+	var s strings.Builder
+
+	s.WriteString(fmt.Sprintf("[%d/%d]\n\n", m.idx+1, m.total))
+	s.WriteString(fmt.Sprintf("File: %s\n\n", m.names[m.idx]))
+	s.WriteString("[K]eep, [D]elete, [S]kip, [Q]uit? ")
+
+	return s.String()
+}
+
+func (m Model) processingView() string {
+	var s strings.Builder
+
+	s.WriteString(fmt.Sprintf("[%d/%d]\n\n", m.idx+1, m.total))
+	s.WriteString(fmt.Sprintf("File: %s\n\n", m.names[m.idx]))
+	s.WriteString("Processing...\n")
+
+	return s.String()
+}
+
+func (m Model) endView() string {
+	return fmt.Sprintf("Completed! Processed %d files.\nPress any key to exit...\n", m.total)
+}
+
+func (m Model) errorView() string {
+	var s strings.Builder
+
+	s.WriteString("Error occurred:\n\n")
+	s.WriteString(fmt.Sprintf("%s\n\n", m.errMsg))
+	s.WriteString("Press any key to exit...\n")
+
+	return s.String()
+}
